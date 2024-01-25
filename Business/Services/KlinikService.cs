fc7ac4becb6f2ef35a7308fcce46134df1e33bdb
Çağıl Alsaç <cagilalsac@outlook.com>
2024-01-25 23:43:41 +0300

    Klinik listeleme ve detay ilgili service ve model üzerinden gerçekleştirildi. HTML ve Tag Helper'lardan bahsedildi.

---
+++ Business/Services/KlinikService.cs
@@ -0,0 +1,31 @@
+﻿using Business.Models;
+using DataAccess.Contexts;
+
+namespace Business.Services
+{
+    public interface IKlinikService
+    {
+        IQueryable<KlinikModel> Query();
+    }
+
+    public class KlinikService : IKlinikService
+    {
+        private readonly Db _db;
+
+        public KlinikService(Db db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<KlinikModel> Query()
+        {
+            return _db.Klinikler.OrderBy(k => k.Adi).Select(k => new KlinikModel() // k: Klinikler'deki her bir Klinik tipindeki eleman delegesi
+            {
+                Aciklamasi = k.Aciklamasi,
+                Adi = k.Adi,
+                Guid = k.Guid,
+                Id = k.Id
+            }); 
+        }
+    }
+}
